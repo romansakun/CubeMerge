93644415db0e46f81cf869a38a93d82edd18e842
roman.sakun <roman.sakun.88@gmail.com>
2023-02-14 08:12:17 +0300

    update LinkerUtility

--- Assets/LinkerUtility/Runtime/ILinkerNode.cs
+++ Assets/LinkerUtility/Runtime/ILinkerNode.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+
 namespace LinkerUtility.Runtime
 {
     public interface ILinkerNode
     {
+        List<ILinkerNode> NextNodes { get; }
         ILinkerNode Next();
         bool HasNext();
     }
--- Assets/LinkerUtility/Runtime/LinkerNode.cs
+++ Assets/LinkerUtility/Runtime/LinkerNode.cs
@@ -2,48 +2,43 @@ using System.Collections.Generic;
 
 namespace LinkerUtility.Runtime
 {
-    public class LinkerNode : ILinkerNode
+    public abstract class LinkerNode : ILinkerNode
     {
-        private readonly List<ILinkerNode> _nodes = new List<ILinkerNode>();
+        private readonly List<ILinkerNode> _nextNodes = new List<ILinkerNode>();
         private readonly Stack<ILinkerNode> _stack = new Stack<ILinkerNode>();
-        private readonly Stack<ILinkerNode> _buffStack = new Stack<ILinkerNode>();
 
-        public void AddNode(ILinkerNode node)
-        {
-            _nodes.Add(node); 
-            _stack.Clear();
-            for (int i = _nodes.Count - 1; i >= 0 ; i--)
-                _stack.Push(_nodes[i]);
-        }
+        public List<ILinkerNode> NextNodes => _nextNodes;
         
-        public void RemoveNode(ILinkerNode node)
-        {
-            _nodes.Remove(node);
-            _stack.Clear();
-            for (int i = _nodes.Count - 1; i >= 0 ; i--)
-                _stack.Push(_nodes[i]);
-        }
 
+        public void AddNext(ILinkerNode node)
+        {
+            _nextNodes.Add(node); 
+            _stack.Clear();
+            for (int i = _nextNodes.Count - 1; i >= 0 ; i--)
+                _stack.Push(_nextNodes[i]);
+        }
+     
         public ILinkerNode Next()
         {
             if (!HasNext())
                 return null;
 
             var node = _stack.Pop();
-            
-            _buffStack.Clear();
-            if (node.HasNext())
-                _buffStack.Push(node.Next());
 
-            while (_buffStack.Count > 0)
-                _stack.Push(_buffStack.Pop());
+            int nodeIndex = node.NextNodes.Count - 1;
+            while (nodeIndex >= 0)
+            {
+                var last = node.NextNodes[nodeIndex];
+                _stack.Push(last);
+                nodeIndex--;
+            }
             
             return node;
         }
 
         public bool HasNext()
         {
-            return _stack.Count != 0;
+            return _stack.Count > 0;
         }
     }
 }
